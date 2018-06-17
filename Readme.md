ae681431bc14a0d2ce942fb8394f343e78bb41ed
JoseChirinos <josealexcba@gmail.com>
2018-06-17 14:07:00 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# GooArt®2018
+
+## GooArt, the best template available only for goomovil®2018
+
+> Instalar Browser-Sync para cambios en tiempo real
+```sh
+    npm install -g browser-sync
+```
+> Para ejecutar el proyecto modo desarrollo
+```sh
+    npm start
+```
---
+++ templates/search/search.js
@@ -0,0 +1,20 @@
+/*
+var cont = 0;
+document.body.onscroll = function(){
+  if(window.scrollY>150){
+    cont++;
+    status = true;
+    if(cont == 1){
+      document.querySelector('.Filter').style.position = 'fixed';
+    }
+  }
+  else{
+    if(cont>0){
+      cont = 0;
+    }
+    if(cont == 0){
+      document.querySelector('.Filter').style.position = 'relative';
+    }
+  }
+}
+*/
