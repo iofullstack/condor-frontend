a7aaa9d464bafc78921caad9171b3aaf83599905
JoseChirinos <josealexcba@gmail.com>
2018-07-02 21:47:07 -0400

    card ready

--- src/app/home/home-app.js
+++ src/app/home/home-app.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import styles from './home-app-styles';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import EditIcon from '@material-ui/icons/Edit';
-import MenuIcon from '@material-ui/icons/Menu';
+import FastFoodIcon from '@material-ui/icons/FastFood';
+import Divider from '@material-ui/core/Divider';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class HomeApp extends Component{
   render(){
+    const { classes } = this.props;
     return(
-      <div>
-        Home: Test of Material UI for React
+      <div className="container-fluid">
+        <h1>Home: Test of Material UI for React</h1>
         <Button>
           Entry to menu
         </Button>
@@ -22,11 +30,59 @@ class HomeApp extends Component{
         <Button variant="fab" color="secondary" aria-label="edit">
           <EditIcon/>
         </Button>
+        <h1>Cards</h1>
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <Card className={classes.card}>
+                <a href="#goto" className={classes.linker}></a>
+                <CardContent>
+                  <Typography className={classes.title} color="textSecondary">
+                    Ventas
+                  </Typography>
+                  <Divider/>
+                  
+                  <FastFoodIcon className={classes.icon}/>
+                  <Typography className={classes.description} component="p">
+                    Realizar una venta o pedido, designar una mesa.
+                  </Typography>
+                </CardContent>
+              </Card>
 
-        <MenuIcon/>
+              <Card className={classes.card}>
+                <a href="#goto" className={classes.linker}></a>
+                <CardContent>
+                  <Typography className={classes.title} color="textSecondary">
+                    Ventas
+                  </Typography>
+                  <Divider/>
+                  
+                  <FastFoodIcon className={classes.icon}/>
+                  <Typography className={classes.description} component="p">
+                    Realizar una venta o pedido, designar una mesa.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+            <div className="col-md-6 col-md-offset-3">
+              <Card className={classes.card}>
+                <a href="#goto" className={classes.linker}></a>
+                <CardContent>
+                  <Typography className={classes.title} color="textSecondary">
+                    Ventas
+                  </Typography>
+                  <Divider/>
+                  
+                  <FastFoodIcon className={classes.icon}/>
+                  <Typography className={classes.description} component="p">
+                    Realizar una venta o pedido, designar una mesa.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
       </div>
     )
   }
 }
 
-export default HomeApp;
+export default withStyles(styles)(HomeApp);
